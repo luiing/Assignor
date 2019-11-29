313d3a4e2bf16358bef575a2141dc2aba46925a4
uis <huaibing.liu@bl.com>
2019-11-29 17:14:48 +0800

    change name

--- assignor/src/main/java/com/uis/assignor/BodyStore.kt
+++ assignor/src/main/java/com/uis/assignor/BodyStore.kt
@@ -10,7 +10,7 @@ import android.support.v4.util.ArrayMap
 import com.uis.assignor.utils.TypeConvert
 
 class BodyStore(@Volatile var mState:Int = State_Created) :IState{
-    private var models:ArrayMap<String,BodyDataContainer> = ArrayMap()
+    private var models:ArrayMap<String,BodyModel> = ArrayMap()
 
     override fun onStateChanged(state: Int) {
         mState = state
@@ -23,14 +23,14 @@ class BodyStore(@Volatile var mState:Int = State_Created) :IState{
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun <B :BodyDataContainer> get(f:(B)->Unit,name:String="") :B =  getModel(TypeConvert.convert(f) as Class<out BodyDataContainer>,name)as B
+    fun <B :BodyModel> get(f:(B)->Unit, name:String="") :B =  getModel(TypeConvert.convert(f) as Class<out BodyModel>,name)as B
 
     @Suppress("UNCHECKED_CAST")
-    fun <B :BodyDataContainer> get(cls:Class<B>,name:String="") :B = getModel(cls,name) as B
+    fun <B :BodyModel> get(cls:Class<B>, name:String="") :B = getModel(cls,name) as B
 
-    fun remove(cls:Class<out BodyDataContainer>,name:String="") = models.remove(getModelName(cls,name))
+    fun remove(cls:Class<out BodyModel>, name:String="") = models.remove(getModelName(cls,name))
 
-    private fun getModel(cls:Class<out BodyDataContainer>,name:String=""):BodyDataContainer {
+    private fun getModel(cls:Class<out BodyModel>, name:String=""):BodyModel {
         val key = getModelName(cls,name)
         return models[key] ?: cls.newInstance().apply {
             autoFindBodyData()
